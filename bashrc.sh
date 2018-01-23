becbb1c2a91d995166fcaaf25c8d60b7cdda99be
Adriano Gil <adrianogil.san@gmail.com>
2018-01-23 17:27:26 -0400

    Added bashrc config file and hdev aliases

---
+++ bashrc.sh
@@ -0,0 +1,13 @@
+# Add this to your profile file
+# export CONFIG_FILES_PATH=/<PATH>/config-files/
+# source ${CONFIG_FILES_PATH}/bashrc.sh
+
+# Fancy colors
+source ${CONFIG_FILES_PATH}/fancy_colors.sh
+source ${CONFIG_FILES_PATH}/text_utils.sh
+# Git alias
+source ${CONFIG_FILES_PATH}/git_config.sh
+# General alias
+source ${CONFIG_FILES_PATH}/useful_aliases.sh
+# Hdev
+source ${CONFIG_FILES_PATH}/hdev.sh
---
+++ hdev.sh
@@ -0,0 +1,1 @@
+alias random-macaddress="openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//'"
