d96409a3c0879a72959a5893f986fc2edbfda3ff
Adriano Gil <adrianogil.san@gmail.com>
2018-07-06 18:22:16 -0400

    Fixed sed commandd

--- useful_aliases.sh
+++ useful_aliases.sh
@@ -209,7 +209,7 @@ if [[ $0 == *termux* ]]; then
     function fleditor-android()
     {
         real_file_path=$(abspath $1)
-        real_file_path=$(echo $real_file_path | sed "s/data//data//com.termux//files//home//storage//shared/sdcard/g" )
+        real_file_path=$(echo $real_file_path | sed "s@data/data/com.termux/files/home/storage/shared@sdcard@g" )
         echo "Open file "$1"using DroidEdit Free"
         am start -n "com.aor.droidedit/.DroidEditFreeActivity" -d "file://"$real_file_path
     }
