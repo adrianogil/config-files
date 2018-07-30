796fb018af0a4bd01aa39bedaf6cbc53a5ff15dd
Adriano Gil <adrianogil.san@gmail.com>
2018-07-30 19:55:37 -0400

    Added function open-chrome-session

--- useful_aliases.sh
+++ useful_aliases.sh
@@ -186,6 +186,22 @@ function save-chrome-session()
     echo 'Saved'$session_size' open tabs from Google Chrome into file '$session_file
 }
 
+# @tool open-chrome-session Open URLs from a chrome session file
+# Using https://github.com/prasmussen/chrome-cli
+function open-chrome-session()
+{
+    if [ -z "$1" ]
+    then
+        session_file=$(gfind . -name '*.chrome-session' -type f -printf "%-.22T+ %M %n %-8u %-8g %8s %Tx %.8TX %p\n" | sort -r | awk '{print $9}' | head -1)
+    else
+        session_file=$1
+    fi
+    echo 'Loading chrome session file: '$session_file
+    session_size=$(cat $session_file | wc -l)
+    echo 'Trying to open session with '$session_size' saved tabs'
+    cat $session_file | xargs -I {} chrome-cli open {}
+}
+
 # Copy using pv (http://www.ivarch.com/programs/pv.shtml)
 # For more info take a look at: http://www.catonmat.net/blog/unix-utilities-pipe-viewer/
 function cpv
