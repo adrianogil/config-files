5e1805fa193babd417b18fae13a3b2eba03dc405
Adriano Gil <adrianogil.san@gmail.com>
2018-11-15 18:59:54 -0400

    Improved simple server with multithreading capabilities

--- python/simpleserver/CustomHTTPServer.py
+++ python/simpleserver/CustomHTTPServer.py
@@ -305,10 +305,44 @@ class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         '.h': 'text/plain',
         })
 
+try:
+    # Python 2.x
+    from SocketServer import ThreadingMixIn
+    from BaseHTTPServer import HTTPServer
+except ImportError:
+    # Python 3.x
+    from socketserver import ThreadingMixIn
+    from http.server import HTTPServer
 
-def test(HandlerClass = SimpleHTTPRequestHandler,
-         ServerClass = BaseHTTPServer.HTTPServer):
-    BaseHTTPServer.test(HandlerClass, ServerClass)
+class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
+    pass
+
+if sys.argv[1:]:
+    address = sys.argv[1]
+    if (':' in address):
+        interface = address.split(':')[0]
+        port = int(address.split(':')[1])
+    else:
+        interface = '0.0.0.0'
+        port = int(address)
+else:
+    port = 8000
+    interface = '0.0.0.0'
+
+if sys.argv[2:]:
+    os.chdir(sys.argv[2])
+
+print 'Started HTTP server on ' +  interface + ':' + str(port)
+
+
+def run_server():
+    server = ThreadingSimpleServer((interface, port), SimpleHTTPRequestHandler)
+    try:
+        while 1:
+            sys.stdout.flush()
+            server.handle_request()
+    except KeyboardInterrupt:
+        print 'Finished.'
 
 if __name__ == '__main__':
-    test()
+    run_server()
