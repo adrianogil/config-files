60edbc1e4b5c32149f255ab891115cde7b2509e0
Adriano Gil <adrianogil.san@gmail.com>
2018-12-07 19:36:42 -0400

    Added video-cut tool

--- useful_aliases.sh
+++ useful_aliases.sh
@@ -250,7 +250,7 @@ function lsort()
     else
         target_directory=$2
     fi
-    
+
     gfind "$target_directory" -name "$target_name" -type f -printf "%-.22T+ %M %n %-8u %-8g %8s %Tx %.8TX %p\n" | sort -r | awk '{print $1"\t"$9}'
 }
 
@@ -288,9 +288,20 @@ function rnd-line()
 
 alias youtube-dl-mp3='youtube-dl -x --audio-format "mp3" '
 
+
 function youtube-dl-mp3-from-playlist()
 {
     youtube-dl -j --flat-playlist $1 | jq -r '.id' | sed 's_^_https://youtube.com/v/_' | cut -c9- | xa youtube-dl  -x --audio-format "mp3" {}
+}
+
+# Based on https://stackoverflow.com/questions/18444194/cutting-the-videos-based-on-start-and-end-time-using-ffmpeg
+function video-cut()
+{
+    target_video=$1
+    output_video=$2
+    start_time=$3    # 00:00:03
+    duration_time=$4 # 00:00:08
+    ffmpeg -i $target_video -ss $start_time -t $duration_time -async 1 $output_video
 }
 
 # https://www.omgubuntu.co.uk/2016/08/learn-new-word-terminal
