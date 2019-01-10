52cbd05967ea331b8d3ebb0a0f850bd7b0b7704c
Adriano Gil <adrianogil.san@gmail.com>
2019-01-10 14:28:53 -0400

    Added a movie to test netflix control

--- termux/netflix/netflix_control.sh
+++ termux/netflix/netflix_control.sh
@@ -1,10 +1,19 @@
 
+NETFLIX_KIDS_MOVIE_IDS_FILE=termux/netflix/netflix_kids_movies_ids.txt
+NETFLIX_MOVIE_IDS_FILE=termux/netflix/netflix_movies_ids.txt
 
 function netflix-rnd()
 {
     # You should define a txt file with list of NETFLIX movies ids
     # NETFLIX_MOVIE_IDS_FILE
-    open-url "http://www.netflix.com/title/"$(shuf -n1 $NETFLIX_MOVIE_IDS_FILE)
+    open-url "http://www.netflix.com/title/"$(shuf -n1 $NETFLIX_MOVIE_IDS_FILE | awk '{print $1}')    
+}
+
+function netflix-rnd()
+{
+    # You should define a txt file with list of NETFLIX movies ids
+    # NETFLIX_MOVIE_IDS_FILE
+    open-url "http://www.netflix.com/title/"$(shuf -n1 $NETFLIX_KIDS_MOVIE_IDS_FILE | awk '{print $1}')
 }
 
 
