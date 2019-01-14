ea97eea2b802c82ce7639cae0093856bd9f76cf1
Adriano Gil <adrianogil.san@gmail.com>
2019-01-14 13:32:43 -0400

    Added function to open chrome-session on Android device

--- osx/browser.sh
+++ osx/browser.sh
@@ -36,6 +36,23 @@ function open-chrome-session()
     cat $session_file | xargs -I {} chrome-cli open {}
 }
 
+# @tool open-chrome-session Open URLs from a chrome session file
+# Using https://github.com/prasmussen/chrome-cli
+function open-chrome-session-droid()
+{
+    if [ -z "$1" ]
+    then
+        session_file=$(gfind . -name '*.chrome-session' -type f -printf "%-.22T+ %M %n %-8u %-8g %8s %Tx %.8TX %p\n" | sort -r | awk '{print $9}' | head -1)
+    else
+        session_file=$1
+    fi
+    echo 'Loading chrome session file: '$session_file
+    session_size=$(cat $session_file | wc -l)
+    echo 'Trying to open session with '$session_size' saved tabs'
+    for u in `cat $session_file`; do echo "$u"; droid-open-url "$u"; sleep 1; done
+}
+
+
 function browser-current-window()
 {
     current_tab=$(chrome-cli info | head -1 | awk '{print $2}')
