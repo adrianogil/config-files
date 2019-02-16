4ebdd3b8d4d07dbae8e63b924ab21dc1d9d480e6
Adriano Gil <adrianogil.san@gmail.com>
2019-02-16 09:03:51 -0400

    Added alias to list dmg files on osx

--- osx/useful_aliases.sh
+++ osx/useful_aliases.sh
@@ -7,3 +7,12 @@ alias shuf='gshuf'
 alias open-url='open'
 
 alias ot='open -a Terminal'
+
+function dmgs()
+{
+    if [[ $1 == "-d" ]]; then
+        gfind . -name '*.dmg' -type f -printf "%-.22T+ %M %n %-8u %-8g %8s %Tx %.8TX %p\n" | sort -r | awk '{print $9"\t"$1}'
+    else
+        find . -name '*.dmg'
+    fi
+}
