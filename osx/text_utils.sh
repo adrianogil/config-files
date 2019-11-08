d19d2f97f6e038b718605ad1cc43a7ee6397c6dc
Adriano Gil <adrianogil.san@gmail.com>
2019-11-08 07:38:49 -0400

    Added functions to create new files and commit them

--- osx/text_utils.sh
+++ osx/text_utils.sh
@@ -1,8 +1,25 @@
-# Send text content to Sublime buffer
 function sublimeb
 {
+    # Send text content to Sublime buffer
     sublime --command 'insert_snippet {"contents" : "'$1'"}'
 }
 
 alias s="sublime"
 alias sw="sublime -n"
+
+function sg()
+{
+    # Open new file into sublime and add it to git
+    new_file=$1
+    touch ${new_file}
+    git add ${new_file}
+    sublime ${new_file}
+}
+
+function tg()
+{
+    # Create new file and add it to git
+    new_file=$1
+    touch ${new_file}
+    git add ${new_file}
+}
