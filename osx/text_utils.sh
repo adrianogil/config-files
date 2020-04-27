699f40d7c720279db616683fcdced8ce09c23cd5
Adriano Gil <adrianogil.san@gmail.com>
2020-04-27 08:53:52 -0400

    Minor improvements regarding test manipulation functions

--- osx/text_utils.sh
+++ osx/text_utils.sh
@@ -7,19 +7,23 @@ function sublimeb
 alias s="sublime"
 alias sw="sublime -n"
 
-function sg()
-{
-    # Open new file into sublime and add it to git
-    new_file=$1
-    touch ${new_file}
-    git add ${new_file}
-    sublime ${new_file}
-}
-
 function tg()
 {
     # Create new file and add it to git
     new_file=$1
+
+    file_directory=$(dirname ${new_file})
+    mkdir -p ${file_directory}
+
     touch ${new_file}
     git add ${new_file}
+}
+
+function sg()
+{
+    # Open new file into sublime and add it to git
+    new_file=$1
+
+    tg ${new_file}
+    sublime ${new_file}
 }
