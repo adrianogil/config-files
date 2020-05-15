7f1e6eff1414e88b3d023dd1b6dd101e004dc2c7
Adriano Gil <adrianogil.san@gmail.com>
2020-05-14 22:27:54 -0400

    Added a function to get the newest fork of a github repo

--- cross/github.sh
+++ cross/github.sh
@@ -7,3 +7,9 @@ function github-save-repos-data()
     target_file=$1
     git hub repos > ${target_file}
 }
+
+function github-get-newest-fork()
+{
+    target_repo=$1
+    curl -X GET https://api.github.com/repos/${target_repo}/forks?sort=newest
+}
