8a98aa6d736f9edb5f772e660affeaf256d44bae
Adriano Gil <adrianogil.san@gmail.com>
2022-11-12 09:51:30 -0400

    Improvements on gen-matrix
    - Added a custom string as spacing

--- python/clitools/gen_matrix.py
+++ python/clitools/gen_matrix.py
@@ -5,6 +5,8 @@ height = sys.argv[2]
 
 equation = sys.argv[3]
 
+spacing = sys.argv[4] if len(sys.argv) > 4 else " "
+
 code = """
 from random import randint
 
@@ -16,8 +18,8 @@ for i in range(0, height):
     s = ""
     for j in range(0, width):
         a = %s
-        s += str(a) + " "
+        s += str(a) + "%s"
     print(s)
 
-""" % (width, height, equation)
+""" % (width, height, equation, spacing)
 exec(code)
