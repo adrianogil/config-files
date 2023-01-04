f2973e471dde5f50329b11da1f83c58b187c2cfa
Adriano Gil <adrianogil.san@gmail.com>
2023-01-04 09:19:03 -0400

    Added some jabba-related functions

--- cross/dev_tools.sh
+++ cross/dev_tools.sh
@@ -16,3 +16,24 @@ function sha()
     echo 'Running '${target_shellscript}
     ${target_shellscript}
 }
+
+function jabba-check-instslled-versions()
+{
+    jabba ls    
+}
+
+
+function jabba-install-java()
+{
+    jabba install $(jabba ls-remote | default-fuzzy-finder)
+}
+
+function jabba-uninstall-java()
+{
+    jabba uninstall $(jabba ls | default-fuzzy-finder)
+}
+
+function config-install-jabba()
+{
+    curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
+}
