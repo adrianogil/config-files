a8f72b6719b54cb269c3bdef9c990d687acfe2cf
Adriano Gil <adrianogil.san@gmail.com>
2023-04-09 18:54:09 -0400

    Define only one process_utils file (for both linux and macOS)

--- cross/process_utils.sh
+++ cross/process_utils.sh
@@ -1,4 +1,25 @@
+function ps-suspend()
+{
+    $PID=$1
+    kill -SIGSTOP $PID
+}
 
+function ps-resume()
+{
+    $PID=$1
+    kill -SIGCONT $PID
+}
+
+function ps-pick()
+{
+    target_pid=$(ps aux | default-fuzzy-finder | awk '{print $2}')
+    echo $target_pid | copy-text-to-clipboard
+    echo $target_pid
+}
+
+function ps-monitor()
+{
+    top -pid $(ps-pick)
+}
 
 alias k9="sudo kill -9 "
-
