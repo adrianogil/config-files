afe7ff68ebabca8b4aaabb4b51beced6aa2d7679
Adriano Gil <adrianogil.san@gmail.com>
2023-11-19 11:43:55 -0400

    refactor: moved gen-matrix to MathPlayground repo

--- cross/math_tools.sh
+++ cross/math_tools.sh
@@ -1,5 +1,4 @@
 
 
-alias gen-matrix='python3 ${CONFIG_FILES_DIR}/python/clitools/gen_matrix.py'
 alias time-calc='python3 ${CONFIG_FILES_DIR}/python/clitools/timecalc.py'
 alias plot-cmd='python ${CONFIG_FILES_DIR}/python/plottool/plot_command.py'
