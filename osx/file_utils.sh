a159bcabc791fe111bbc984d3e9c2e5c8e91cf64
Adriano Gil <adrianogil.san@gmail.com>
2024-08-04 21:39:40 -0400

    feat: Add functions to list and remove .DS_Store files

--- osx/file_utils.sh
+++ osx/file_utils.sh
@@ -1,1 +1,13 @@
 alias o="open"
+
+# config-tools ds_store: list .DS_Store files
+function ds_store()
+{
+    find . -name ".DS_Store"
+}
+
+# config-tools ds_store-rm: remove .DS_Store files
+function ds_store-rm()
+{
+    find . -name ".DS_Store" -exec rm -f {} \;
+}
