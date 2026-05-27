2e90028ed572ed0ac058b82d5c5a6435c9416eb6
Adriano Gil <adrianogil.san@gmail.com>
2026-05-27 19:40:24 -0400

    Improve sd alias detection

--- linux/general_utils.sh
+++ linux/general_utils.sh
@@ -2,16 +2,28 @@
 
 function see-definition()
 {
-    # Show the definition of a target function
+    # Show the definition of a target alias/function
 
     if [ -z "$1" ]
     then
-        # zsh
-        read -p "Enter target function: " target_function
+        printf "Enter target function: "
+        read -r target_function
     else
         target_function=$1
     fi
 
-    type ${target_function}
+    if alias "${target_function}" >/dev/null 2>&1
+    then
+        echo "${target_function} is an alias"
+        alias "${target_function}"
+        return
+    fi
+
+    if type whence >/dev/null 2>&1
+    then
+        whence -f "${target_function}"
+    else
+        type "${target_function}"
+    fi
 }
 alias sd="see-definition"
--- osx/general_utils.sh
+++ osx/general_utils.sh
@@ -1,19 +1,30 @@
 
 
-# config-tools see-definition: Show the definition of a target function
+# config-tools see-definition: Show the definition of a target alias/function
 function see-definition()
 {
-    # Show the definition of a target function
+    # Show the definition of a target alias/function
 
     if [ -z "$1" ]
     then
-        # zsh
-        echo "Enter target function: " 
-        read target_function
+        printf "Enter target function: "
+        read -r target_function
     else
         target_function=$1
     fi
 
-    whence -f ${target_function}
+    if alias "${target_function}" >/dev/null 2>&1
+    then
+        echo "${target_function} is an alias"
+        alias "${target_function}"
+        return
+    fi
+
+    if type whence >/dev/null 2>&1
+    then
+        whence -f "${target_function}"
+    else
+        type "${target_function}"
+    fi
 }
 alias sd="see-definition"
